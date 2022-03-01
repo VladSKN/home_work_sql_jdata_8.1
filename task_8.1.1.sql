ff6a0e6cae01d678fef42c7f19fde86e4f158214
Vlad Semikin <vladsemikin3@gmail.com>
2022-03-01 21:09:34 +0200

    task correction

--- task_8.1.1.sql
+++ task_8.1.1.sql
@@ -1,9 +1,9 @@
 CREATE TABLE PERSONS
 (
-name varchar2(15),
-surname varchar2(15),
-age integer, 
-phone_number integer, 
-city_of_living varchar2(15),
-PRIMARY KEY(name, surname, age)
+    name varchar(15),
+    surname varchar(15),
+    age int,
+    phone_number int,
+    city_of_living varchar(15),
+    PRIMARY KEY(name, surname, age)
 );
